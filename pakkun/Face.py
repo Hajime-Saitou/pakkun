aa4d8f990f24cfadd1633a6819f323b8c88cdcc1
Hajime Saitou <g.hajime-saitou@gmail.com>
2021-10-23 08:33:53 +0900

    キーチェックを追加

--- pakkun/Face.py
+++ pakkun/Face.py
@@ -2,11 +2,11 @@ from .Origin import Origin
 from .Vertices import Vertices
 
 class Face(object):
-    def __init__(self, object, key:int):
+    def __init__(self, object, index:int):
         self.object = object
-        self.name = str(key)
+        self.index = index
         self.origin = Origin(self.object, self)
-        self.face = self.object.data.polygons[key]
+        self.face = self.object.data.polygons[index]
 
     @property
     def vertices(self):
@@ -20,3 +20,7 @@ class Face(object):
     def toLoopIndex(self, vertexIndex:int):
         print(vertexIndex)
         return self.loopIndices[[ index for index in self.face.vertices ].index(vertexIndex)]
+
+    @property
+    def materialIndex(self):
+        return self.face.material_index
--- pakkun/VertexGroups.py
+++ pakkun/VertexGroups.py
@@ -5,4 +5,11 @@ class VertexGroups(object):
         self.object = object
 
     def __getitem__(self, key):
+        if self.object.vertex_groups.find(key) == -1:
+            raise KeyError(f"Undefined vertex group '{key}''.")
+
         return VertexGroup(self.object, key)
+
+    @property
+    def length(self):
+        return len(self.object.vertex_groups)
