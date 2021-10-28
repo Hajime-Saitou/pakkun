664217733eb61f90af75fd2c613cb3da6f6ccfde
Hajime Saitou <g.hajime-saitou@gmail.com>
2021-10-29 06:56:18 +0900

    型ヒント追記

--- pakkun/Origin.py
+++ pakkun/Origin.py
@@ -2,4 +2,4 @@ class Origin(object):
     def __init__(self, origin, accessor):
         self.origin = origin
         self.accessor = accessor
-        self.belongTo = accessor.__class__.__name__
+        self.belongTo:str = accessor.__class__.__name__
